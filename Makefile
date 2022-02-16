dbf8770f3933edd9982e0ffd18e252d1e516467f
Nikita Voitov <pochta5343@mail.ru>
2022-02-16 12:10:18 +0300

    update make,add workflows

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ install:
 	composer install
 dump:
 	composer dump-autoload
+lint:
+	composer exec --verbose phpcs -- --standard=PSR12 src tests
+testing:
+	php ./tests/Test2.php
--- src/StringUtils.php
+++ src/StringUtils.php
@@ -7,7 +7,7 @@ function capitalize($text)
     if ($text === '') {
         return '';
     }
-    $firstSymbol = strtoupper($text[0]);
+    $firstSymbol = mb_strtoupper($text[0]);
     $restSubstring = substr($text, 1);
     return "$firstSymbol$restSubstring";
 }
