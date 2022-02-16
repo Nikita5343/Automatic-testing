d1b165e818b78ba1d5313ce07a51ee5e7be81669
Nikita Voitov <pochta5343@mail.ru>
2022-02-16 12:07:04 +0300

    add require if if...

--- tests/Test2.php
+++ tests/Test2.php
@@ -1,11 +1,23 @@
 <?php
 
-require_once "../vendor/autoload.php";
-require_once "../src/StringUtils.php";
+$patch1 = __DIR__ . '/../vendor/autoload.php';
+$patch2 = __DIR__ . '/../../../autoload.php';
+if (file_exists($patch1)) {
+    require_once $patch1;
+} else {
+    require_once $patch2;
+}
+$src1 = __DIR__ . '/src/StringUtils.php';
+$src2 = __DIR__ . '/../src/StringUtils.php';
+if (file_exists($src1)) {
+    require_once $src1;
+} else {
+    require_once $src2;
+}
 
 use function StringUtils\capitalize;
 use Webmozart\Assert\Assert;
 
-Assert::notEq(capitalize(''),'1');
-Assert::notEq(capitalize('hello'),'Hedllo');
+Assert::notEq(capitalize(''), 'f');
+Assert::notEq(capitalize('hello'), 'Hedllo');
 echo "Тесты прошли!";
